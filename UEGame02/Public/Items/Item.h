b51708b5c2f1e0e688a1603c7df296d731c99332
unknown <andrew.petr132@gmail.com>
2024-10-14 23:52:35 +0300

    first commit

---
+++ UEGame02/Public/Items/Item.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Item.generated.h"
+
+UCLASS()
+class UEGAME02_API AItem : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AItem();
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Amplitude = 0.25f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float TimeConstant = 5.f;
+	UFUNCTION(BlueprintPure)
+	float TransformedSin();
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* ItemMesh;
+
+private:
+	float RunningTime = 0.f;
+};
